39e1374fd56514164200cf97849c4e5c9c127b86
GamerTruce <maleikmiller@gmail.com>
2021-10-24 21:06:37 -0500

    Finalized Javascript File

--- scripts.js
+++ scripts.js
@@ -1,6 +1,29 @@
 /* Add your JavaScript to this file */
 document.addEventListener('DOMContentLoaded', function(){
 
+    var button = document.querySelector('button');
+    
+    var  message = document.querySelector('.message');
+
+    var email = document.getElementById('email');
+
+
+    button.addEventListener('click', function(){
+        event.preventDefault();
+
+        var space = email.value;
+        
+        if(space == ''){
+            event.preventDefault();
+             message.textContent = ("Please enter a valid email address.");
+        }
+        
+        else{
+             message.innerHTML = `Thank you! Your email address ${space} has been added to our mailing list!`;
+        }
+        
+        email.value = '';
+    });
 
 
 
@@ -10,4 +33,5 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
+    
 });
